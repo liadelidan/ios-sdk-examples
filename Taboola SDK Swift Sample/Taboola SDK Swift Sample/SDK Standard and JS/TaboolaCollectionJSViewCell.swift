a85d7db580aea9d21a8ab5e1b23281750dcbd5ff
Liad Elidan <liad.e@taboola.com>
2019-07-21 16:39:29 +0300

    preparing

---
+++ Taboola SDK Swift Sample/Taboola SDK Swift Sample/SDK Standard and JS/TaboolaCollectionJSViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaboolaCollectionJSViewCell.swift
+//  Taboola SDK Swift Sample
+//
+//  Created by Liad Elidan on 21/07/2019.
+//  Copyright © 2019 Taboola LTD. All rights reserved.
+//
+
+import UIKit
+
+class TaboolaCollectionJSViewCell: UICollectionViewCell {
+    
+}
---
+++ Taboola SDK Swift Sample/Taboola SDK Swift Sample/SDK Standard and JS/TaboolaCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaboolaCollectionViewCell.swift
+//  Taboola SDK Swift Sample
+//
+//  Created by Liad Elidan on 21/07/2019.
+//  Copyright © 2019 Taboola LTD. All rights reserved.
+//
+
+import UIKit
+
+class TaboolaCollectionViewCell: UICollectionViewCell {
+    
+}
